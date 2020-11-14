7531300273f352b91ddd73624f1e55832ceb72b6
Aru <aru.yelemes23@gmail.com>
2020-11-14 14:17:09 +0600

    zuul server

---
+++ src/test/java/kz/iitu/Zulu/ZuluApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.iitu.Zulu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
